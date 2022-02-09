aa4855c24b65cc49d22681be27b167f58975f445
Fredy Arciniegas <fredy.arciniegas@pragma.com.co>
2022-02-09 09:44:30 -0500

    se agregan configuraciones de eureka client a los micros de usuario e imagen

--- usuario-service/src/main/java/com/pragma/crecimiento/microservicios/usuario/UsuarioApplication.java
+++ usuario-service/src/main/java/com/pragma/crecimiento/microservicios/usuario/UsuarioApplication.java
@@ -2,8 +2,10 @@ package com.pragma.crecimiento.microservicios.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UsuarioApplication implements WebMvcConfigurer{
 
